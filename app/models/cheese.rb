f61efc78e6ee03966512d7b428812259d53509ee
Gordon Hodanich <ghodanich@gmail.com>
2021-09-09 13:29:42 -0400

    done

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -4,4 +4,20 @@ class Cheese < ApplicationRecord
     "#{name}: $#{price}"
   end
 
+  def show
+    cheese = Cheese.find_by(id: params[:id])
+    render json: {
+      id: cheese.id,
+      name: cheese.name,
+      price: cheese.price,
+      is_best_seller: cheese.is_best_seller
+    }
+  end
+  def index
+    cheeses = Cheese.all
+    render json: cheeses, except: [:created_at, :updated_at]
+  end
+
+  
+
 end
